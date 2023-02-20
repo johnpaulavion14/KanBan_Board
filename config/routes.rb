663f7bcbc0939fa01f0ffafbcf8c0669d9845627
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2023-02-20 10:54:55 +0800

    Add Create Card Feature using scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :cards
   get 'dashboard/index'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
