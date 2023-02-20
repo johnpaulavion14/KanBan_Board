347342d047e3d0e2c2b9a11ad6b309894196c3de
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2023-02-20 14:31:05 +0800

    Add bootstrap design for creating new card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -25,7 +25,7 @@ class CardsController < ApplicationController
 
     respond_to do |format|
       if @card.save
-        format.html { redirect_to card_url(@card), notice: "Card was successfully created." }
+        format.html { redirect_to cards_path, notice: "Card was successfully created." }
         format.json { render :show, status: :created, location: @card }
       else
         format.html { render :new, status: :unprocessable_entity }
