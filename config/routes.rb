c616b9bba9f216f33965ed9155e2243989ccc391
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2023-02-26 14:55:49 +0800

    Add javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: 'users/registrations'  }
 
+  #Card
+  post 'card/create' => 'cards#create', as: 'create_card'
+  
+  #add Addcard
   post 'addcards/:card_id/addcards' => 'addcards#create', as: 'create_addcard'
   get 'card/:card_id/:addcard_id/edit' => 'addcards#edit', as: 'edit_addcard'
   patch 'card/:card_id/addcards/:addcard_id' => 'addcards#update', as: 'update_addcard'
   get 'newcard/:id/:card_title' => 'addcards#new', as: 'newcard'
   delete 'card/:card_id/addcards/:addcard_id' => 'addcards#destroy', as: 'delete_addcard'
+
   resources :addcards
   resources :cards
 
