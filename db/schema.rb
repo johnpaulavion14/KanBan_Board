43e7114191dc68611c819e71fd92caf55275f650
johnpaulavion14 <johnpaul.bocatija.14@gmail.com>
2023-11-03 11:21:15 +0800

    Test commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_023227) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_015536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -204,10 +204,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_023227) do
     t.datetime "start_date"
     t.integer "duration"
     t.integer "parent"
+    t.integer "sortorder", default: 0
     t.decimal "progress"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "sortorder", default: 0
   end
 
   create_table "users", force: :cascade do |t|
